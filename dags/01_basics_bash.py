727e84105ad193a1b917ef1cecdacc26812aa1a7
josiah113366-sketch <josiah113366@gmail.com>
2026-08-12 14:53:33 +0900

    Feat: 오퍼레이터 배치 및 의존성 부여 task 정의함

--- dags/01_basics_bash.py
+++ dags/01_basics_bash.py
@@ -20,9 +20,29 @@ from airflow.operators.bash import BashOperator
 # 스케줄 -> 시간 
 from datetime import datetime, timedelta
 
+# 2-1. default_args, 편의상 바깥에서 정의, 향후 내부에서 정의
+default_args = {
+    "owner"           : "aic-de1-admin",     # DAG 소유주
+    "depends_on_past" : False,               # 과거 데이터(가동 시간 대비) 소급 처리 금지
+    "retries"         : 1,                   # 작업 실패 시 재시도 횟수 1회 설정
+    "retry_delay"     : timedelta(minutes=5) # 작업 실패 후 5분 후 재시도
+    # 시나리오 
+    # 작업 성공 -> 완료
+    # 작업 실패 -> 5분 대기 -> 1회 재시도 -> 성공 -> 완료
+    # 작업 실패 -> 5분 대기 -> 1회 재시도 -> 실패 -> 완료(실패)
+    #             향후 작업이 재개되도(다음 스케줄에 의해) -> 누락된 과거 데이터 소급 X (백필 X)
+}
+
 # 2. DAG 정의 -> DAG 세션이 오픈된다 의미
 with DAG(
-    dag_id = "01_basics_bash" # DAG간 구분하는 용도 
+    dag_id      = "01_basics_bash",  # DAG간 구분하는 용도 
+    description = "DE 업무 중 배치 파이프라인 구성 중 오케스트레이션 담당 airflow의 DAG 작성 기본형",  #DAG 설명
+    default_args = default_args, 
+    schedule_interval = "@daily", # 하루에 한 번 00시 00분 00초, 문자열, cron 표현 (* * * *)
+    start_date = datetime(2026,6,29), # 현재 기준 갭이 발생 -> 소급 처리 X (위 설정 처리 X)
+    catchup = False, # 과거에 대한 소급 처리 실행 방지
+    # 기본 설정에서 소급 X, 오늘 기준으로 8월 13일 00시 00분 00초에 작동
+    tags = ['bash', 'basic'] # DAG 검색(특정)을 위해서 자유롭게 세팅
 ) as dag:
 
     # 3. Operator 정의 
