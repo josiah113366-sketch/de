b2be938e5e8ed212a6ce0b1b5c27d3f93fd7c06e
josiah113366-sketch <josiah113366@gmail.com>
2026-09-16 13:12:40 +0900

    Feat: ETL 처리 함수 골격

--- emr/spark_etl.py
+++ emr/spark_etl.py
@@ -16,3 +16,27 @@ else:
 BUCKET_NAME = "de-ai-16-loggen-s3-bk-827913617635" 
 INPUT_PATH = f"s3://{BUCKET_NAME}/raw_data.json" # 나중에 필요 시 dt = {TARGET_DATE} 파티션 처리 가능함
 OUTPUT_PATH = f"s3://{BUCKET_NAME}/processed/" # 나중에 필요 시 ~/processed/dt = {TARGET_DATE}/
+
+# 4. 스파크를 통한 ETL 처리 함수 -> 브론즈 -> (정제, 처리 시간 기록) ->  실버
+def clean_processing():
+  # 4-1. 스파크 세션 생성
+  spark = (SparkSession 
+          .builder 
+          .appName(f'Daily_Data_Cleaning_{TARGET_DATE}')   # airflow에서 전달된 시간 정보로 앱 이름 구성
+          .getOrCreate()
+          )
+
+  # 4-2. Extract 데이터 추출, 스키마 준비, JSON 경로(브론즈 경로) -> df 
+
+  # 4-3. Transform 정제 -> 필터
+  # 4-4. Transform 파생 변수 -> 처리 시간 기록
+
+  # 4-5. Load parquet로 저장 
+
+  # 4-6. 스파크 세션 종료
+  spark.stop() 
+  pass
+
+# 5. 엔트리 포인트 
+if __name__ == "__main__":
+  clean_processing()
